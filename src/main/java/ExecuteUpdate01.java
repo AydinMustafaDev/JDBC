417d2c1a0185c50dc9763291c4e557cd16b0dc8a
AydinMustafaDev <xaydin35@gmail.com>
2023-06-01 14:12:59 +0300

    30.05.2023

--- src/main/java/ExecuteUpdate01.java
+++ src/main/java/ExecuteUpdate01.java
@@ -5,9 +5,11 @@ public class ExecuteUpdate01 {
         Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "1234");
         Statement statement = connection.createStatement();
 
-        //1. Örnek: number_of_employees değeri ortalama çalışan sayısından az olan number_of_employees değerlerini 16000 olarak UPDATE edin.
+        //1. Örnek: number_of_employees değeri ortalama çalışan sayısından az olan number_of_employees değerlerini
+        // 16000 olarak UPDATE edin.
         String sql = "UPDATE companies SET number_of_employees = 16000 WHERE number_of_employees < (SELECT AVG(number_of_employees) FROM companies)";
-        int guncellenenSatirSayisi = statement.executeUpdate(sql);//executeUpdate() methodu güncellenen satır sayısını int değer olarak döner
+        int guncellenenSatirSayisi = statement.executeUpdate(sql);//executeUpdate() methodu güncellenen satır sayısını
+                                                                  // int değer olarak döner
         System.out.println("guncellenenSatirSayisi = " + guncellenenSatirSayisi);
 
         //Güncelleme sonrası datayı okumak için DQL(Select) kullanıyoruz:
